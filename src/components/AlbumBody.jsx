9bdb905794140e56f1d7ef3e4bb871ca4424f951
Noah Medoff <nmedoff123@gmail.com>
2018-09-09 14:37:36 -0700

    Inirial commit

---
+++ src/components/AlbumBody.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const AlbumBody = () => {
+  return (
+    <div>
+      <div>
+        <li>Artist Name: Tame Impaka</li>
+        <li>Album Name: Lonerism</li>
+      </div>
+      <div>
+        <li>Artist Name: David Bowie</li>
+        <li>Album Name: The Rise and Fall of Ziggy Stardust and the Spiders from Mars</li>
+      </div>
+      <div>
+        <li>Artist: Kids See Ghosts</li>
+        <li>Album: Kids See Ghosts</li>
+      </div>
+    </div>
+  )
+}
+
+export default AlbumBody;
